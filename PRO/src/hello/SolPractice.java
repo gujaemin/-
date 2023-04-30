4a57c66875f2dd0b10df264c887f6fd1c864b1ce
gujaemin <woalsrn@naver.com>
2023-04-30 18:13:39 +0900

    added a

---
+++ PRO/src/hello/SolPractice.java
@@ -0,0 +1,36 @@
+package hello;
+
+import java.util.Scanner;
+
+public class SolPractice {
+	public static void main(String[] argus) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String inputdata;
+		
+		while(true) {
+			
+			inputdata = scanner.next();
+			System.out.println("입력된 문자열 : \"" + inputdata + "\"");
+			if(inputdata.equals("q")) {
+				break;
+				
+				
+	
+			}
+		}
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
